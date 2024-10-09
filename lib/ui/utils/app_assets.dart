aa3ae6830397a44e0e983f5bf9cb4a4c8b671f5e
Abdelrahman Mohamed Foly <124387119+1223355566@users.noreply.github.com>
2024-10-10 01:14:56 +0200

    app structure commit

---
+++ lib/ui/utils/app_assets.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImages1
+  /// assets/images/1.png
+  static const String assetsImages1 = "assets/images/1.png";
+
+  /// Assets for assetsImages2
+  /// assets/images/2.png
+  static const String assetsImages2 = "assets/images/2.png";
+
+  /// Assets for assetsImages3
+  /// assets/images/3.png
+  static const String assetsImages3 = "assets/images/3.png";
+
+  /// Assets for assetsImagesGam3
+  /// assets/images/gam3.svg
+  static const String assetsImagesGam3 = "assets/images/gam3.svg";
+
+  /// Assets for assetsImagesOrder
+  /// assets/images/order.png
+  static const String assetsImagesOrder = "assets/images/order.png";
+
+  /// Assets for assetsImagesOtp
+  /// assets/images/otp.png
+  static const String assetsImagesOtp = "assets/images/otp.png";
+
+  /// Assets for assetsImagesPyramids
+  /// assets/images/pyramids.svg
+  static const String assetsImagesPyramids = "assets/images/pyramids.svg";
+}
+
---
+++ lib/ui/utils/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0xFFC49E85);
+  static const Color secondaryColor = Color(0xFFF8F4F9);
+  static const Color deepBrownColor = Color(0xFF6B4B3E);
+  static const Color grayColor = Color(0xFF6F6460);
+  static const Color darkColor = Color(0xFF000000);
+  static const Color whiteColor = Color(0xFFfffffff);
+}
